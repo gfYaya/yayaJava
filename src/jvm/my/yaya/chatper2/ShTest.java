48a790d8d9a7d066b1e682f20a6e51f01cc45e17
Yaya <2645919729@qq.com>
2018-02-10 13:30:03 +0800

    jvm test

---
+++ src/jvm/my/yaya/chatper2/ShTest.java
@@ -0,0 +1,13 @@
+package jvm.my.yaya.chatper2;
+
+/**
+ * Java虚拟机规范2.11.3
+ * 位或指令与 位与指令 只有i和l 是否只能为整形才能使用位运算?
+ */
+public class ShTest {
+    public static void main(String[] args) {
+        float a= 1.0f;
+        double b =2.0;
+        // int i = a & b; //不行,只能为整形类型
+    }
+}
