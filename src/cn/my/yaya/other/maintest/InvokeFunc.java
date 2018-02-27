71016eef518d88e0dcd5987852bc45d9bb0503b3
gfYaya <2645919729@qq.com>
2018-02-27 19:29:48 +0800

    other exec

---
+++ src/cn/my/yaya/other/maintest/InvokeFunc.java
@@ -0,0 +1,7 @@
+package cn.my.yaya.other.maintest;
+
+public class InvokeFunc {
+    public static void main(String[] args) {
+        System.out.println("This is InvokeFunc");
+    }
+}
---
+++ src/cn/my/yaya/other/maintest/MainTest.java
@@ -0,0 +1,10 @@
+package cn.my.yaya.other.maintest;
+
+//测试一个类的main方法 能否调用另一个类的 main方法
+//result : 可以的
+public class MainTest {
+    public static void main(String[] args) {
+        System.out.println("Main starting..");
+        InvokeFunc.main(args);
+    }
+}
