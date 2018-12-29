45b336e8bd547792348142b25b67e60ae93417cc
gfYaya <2645919729@qq.com>
2018-12-29 17:55:07 +0800

    netty server 提交

--- src/cn/netty/chapter2/EchoClientHandler.java
+++ src/cn/netty/chapter2/EchoClientHandler.java
@@ -4,31 +4,40 @@ import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.util.CharsetUtil;
 
 /*
  * P17 并根据netty 的github项目进行自改,进行完整测试,补全部分demo 参考netty github的example
  */
-public class EchoClientHandler extends ChannelInboundHandlerAdapter {
-    private final ByteBuf firstMessage;
+public class EchoClientHandler extends SimpleChannelInboundHandler<ByteBuf> {
+    //private final ByteBuf firstMessage;
 
     /**
      * Creates a client-side handler.
      */
     public EchoClientHandler() {
-        firstMessage = Unpooled.buffer(EchoClient.SIZE);
+        /*firstMessage = Unpooled.buffer(EchoClient.SIZE);
         for (int i = 0; i < firstMessage.capacity(); i ++) {
             firstMessage.writeByte((byte) i);
-        }
+        }*/
     }
 
     @Override
     public void channelActive(ChannelHandlerContext ctx) {
-        ctx.writeAndFlush(firstMessage);
+        //ctx.writeAndFlush(firstMessage);
+        ctx.writeAndFlush(Unpooled.copiedBuffer("Netty rocks!",
+                CharsetUtil.UTF_8));
     }
 
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) {
         ctx.write(msg);
+    }
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext channelHandlerContext, ByteBuf byteBuf) throws Exception {
+        System.out.println("Client received: " + byteBuf.toString(CharsetUtil.UTF_8));
     }
 
     @Override
