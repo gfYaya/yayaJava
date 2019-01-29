ede9eb7e253374233c27a1ae335ea446ad5f3094
gfYaya <2645919729@qq.com>
2019-01-29 15:03:16 +0800

    netty file ,SSL与zero-copy 解释

--- src/cn/netty/exmaple/file/FileServerHandler.java
+++ src/cn/netty/exmaple/file/FileServerHandler.java
@@ -27,7 +27,11 @@ public class FileServerHandler extends SimpleChannelInboundHandler<String> {
         }
 
         ctx.write("OK: " + raf.length() +'\n');
-        //不开启SSL 才能使用零拷贝?
+        //不开启SSL 才能使用零拷贝?  =>零拷贝数据加密或者压缩的文件系统是不可用的——只能传输文件的原始内容
+        /* 大宽宽:
+            任何要对数据作加工的都不可以,比如你用http，但是却想对数据做个zip压缩,如果你的原始数据不是压缩的，而想在输出到socket时实时压缩，
+            就不可以但如果你一开始就压缩好了，存文件系统里，就可以了.
+         */
         if(ctx.pipeline().get(SslHandler.class) != null){
             //SSL not enabled - can use zero-copy file transfer
             /** class DefaultFileRegion :
