9d950e4509813acfff458e8a696393fc46fa6b14
gfYaya <2645919729@qq.com>
2019-02-19 22:49:02 +0800

    netty example 家里的 snoop

--- src/cn/netty/exmaple/http/snoop/HttpSnoopClient.java
+++ src/cn/netty/exmaple/http/snoop/HttpSnoopClient.java
@@ -1,4 +1,18 @@
 package cn.netty.exmaple.http.snoop;
 
-public class HttpSnoopClient {
+import java.net.URI;
+import java.net.URISyntaxException;
+
+/**
+ * A simple HTTP client that prints out the content of the HTTP response to
+ * {@link System#out} to test {@link HttpSnoopServer}.
+ */
+public final class HttpSnoopClient {
+
+    static final String  URL = System.getProperty("url", "http://127.0.0.1/8080");
+
+    public static void main(String[] args) throws Exception {
+        //Represents a Uniform Resource Identifier (URI) reference.
+        URI uri  = new URI(URL);
+    }
 }
--- src/cn/netty/exmaple/http/snoop/HttpSnoopClientInitializer.java
+++ src/cn/netty/exmaple/http/snoop/HttpSnoopClientInitializer.java
@@ -1,4 +1,40 @@
 package cn.netty.exmaple.http.snoop;
 
-public class HttpSnoopClientInitializer {
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.http.HttpClientCodec;
+import io.netty.handler.codec.http.HttpContentDecompressor;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.ssl.SslContext;
+
+public class HttpSnoopClientInitializer extends ChannelInitializer<SocketChannel> {
+
+    private final SslContext sslCtx;
+
+    public HttpSnoopClientInitializer(SslContext sslCtx) {
+        this.sslCtx = sslCtx;
+    }
+
+    @Override
+    protected void initChannel(SocketChannel ch) throws Exception {
+        System.out.println("client initializer initChannel");
+        ChannelPipeline p = ch.pipeline();
+
+        //Ennable HTTPS  if nessary
+        if(sslCtx != null){
+            p.addLast(sslCtx.newHandler(ch.alloc()));
+        }
+        p.addLast(new HttpClientCodec());
+
+        //remove the following line if you don't want atomic content decompression
+        p.addLast(new HttpContentDecompressor());
+
+        //Uncomment the following line if you don't want to hanlde HttpContents.
+        //p.addLast(new HttpObjectAggregator(1048576));
+
+        p.addLast(new HttpSnoopClientHandler());
+    }
+
 }
